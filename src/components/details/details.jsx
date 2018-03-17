6e62e3fdac675dc9cde4498ff42de7b6ccaa5d3c
walshsp87 <walshsp87@gmail.com>
2018-03-17 12:07:33 -0500

    Listed details data on details.jsx

---
+++ src/components/details/details.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+export const Details = ({ close, name }) => (
+    <div>
+        <div>
+            <span onClick={ close }>Back/Close</span>
+        </div>
+        <div>
+            <div>ID: { name.id }</div>
+            <div>NAME: { name.name }</div>
+            <div>GENDER: { name.gender }</div>
+            <div>ORIGIN: { name.origin }</div>
+            <div>PRONUNCIATION: { name.pronunciation }</div>
+            <div>MEANING: { name.meaning }</div>
+            <div>HISTORY: { name.history }</div>
+            <div>SIMILAR: { name.similar.map(similarNamesMap) }</div>
+        </div>
+    </div>
+);
+
+function similarNamesMap(name) {
+    return (
+        <div>{name}</div>
+    )
+}
